f2e92e52aab43053d7fa9a9341eeaa5e488f5cc0
emilijajevremovic <emilijajevremovic@gmail.com>
2024-11-24 21:35:07 +0100

    Prvi commit

---
+++ app/frontend/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+
+export const routes: Routes = [{ path: 'login', component: LoginComponent }];
