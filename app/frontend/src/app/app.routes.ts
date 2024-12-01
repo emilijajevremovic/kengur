47edcfbda92af1fed9b72ac005e95ee4600e26b4
Emilija Jevremovic <jev.emilija@gmail.com>
2024-12-01 16:20:52 +0100

    register strana

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-export const routes: Routes = [{ path: 'login', component: LoginComponent }];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent }, 
+    { path: 'register', component: RegisterComponent }, 
+];
