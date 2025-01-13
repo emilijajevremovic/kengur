8b207cdc6a53a0f5d7e17b008d7ae740951f2696
Emilija Jevremovic <jev.emilija@gmail.com>
2025-01-13 22:52:03 +0100

    zapocet send request popup

--- app/frontend/src/app/components/friend-requests/friend-requests.component.ts
+++ app/frontend/src/app/components/friend-requests/friend-requests.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-friend-requests',
   standalone: true,
-  imports: [RouterModule, NavbarComponent],
+  imports: [RouterModule, NavbarComponent, NgIf, CommonModule],
   templateUrl: './friend-requests.component.html',
   styleUrl: './friend-requests.component.scss'
 })
 export class FriendRequestsComponent {
+  isPopupOpen = false; // Kontroliše da li je popup otvoren
 
+  openPopup() {
+    this.isPopupOpen = true; // Otvori popup
+  }
+
+  closePopup() {
+    this.isPopupOpen = false; // Zatvori popup
+  }
 }
