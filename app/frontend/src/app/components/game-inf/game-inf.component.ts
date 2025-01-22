57c81de4453f8295a0ccf335dcd376f4e3a1dc27
Emilija Jevremovic <jev.emilija@gmail.com>
2025-01-22 09:01:29 +0100

    integracija codemirror-a u projekat

--- app/frontend/src/app/components/game-inf/game-inf.component.ts
+++ app/frontend/src/app/components/game-inf/game-inf.component.ts
@@ -1,20 +1,39 @@
 import { CommonModule, NgFor, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import * as CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css'; 
 
 @Component({
   selector: 'app-game-inf',
   standalone: true,
   imports: [NgIf, NgFor, CommonModule, RouterModule, FormsModule],
   templateUrl: './game-inf.component.html',
-  styleUrl: './game-inf.component.scss'
+  styleUrls: ['./game-inf.component.scss']
 })
-export class GameInfComponent implements OnInit {
-
+export class GameInfComponent implements OnInit, AfterViewInit {
   startDate: Date | null = null;
   endDate: Date | null = null;
   duration: string = '';
+
+  @ViewChild('editor') editorElement!: ElementRef;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (typeof window !== 'undefined') {
+      import('codemirror').then(CodeMirror => {
+        CodeMirror.fromTextArea(this.editorElement.nativeElement, {
+          lineNumbers: true,     // Prikazuje brojeve linija
+          mode: 'javascript',    // Sintaksno isticanje za JavaScript
+          theme: 'default',      // Tema
+        });
+      });
+    }
+  }
+  
 
   ngOnInit() {
     this.startDate = new Date();
@@ -38,7 +57,6 @@ export class GameInfComponent implements OnInit {
     this.endDate = new Date(); 
     console.log('End time:', this.endDate);
     this.calculateDuration();
-    //console.log(this.duration);
   }
 
   calculateDuration(): void {
