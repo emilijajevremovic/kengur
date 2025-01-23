6bf1366ffaaf628bff40435eb01f315c9f20780b
Emilija Jevremovic <jev.emilija@gmail.com>
2025-01-23 18:32:01 +0100

    izgled game-result strane

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { FriendRequestsComponent } from './components/friend-requests/friend-requests.component';
 import { GameMathComponent } from './components/game-math/game-math.component';
 import { GameInfComponent } from './components/game-inf/game-inf.component';
+import { GameResultComponent } from './components/game-result/game-result.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent }, 
@@ -11,4 +13,6 @@ export const routes: Routes = [
     { path: 'lobby', component: FriendRequestsComponent }, 
     { path: 'game-math', component: GameMathComponent }, 
     { path: 'game-informatics', component: GameInfComponent }, 
+    { path: 'game-result', component: GameResultComponent }, 
+    { path: 'profile', component: ProfileComponent }, 
 ];
--- app/frontend/src/app/components/game-result/game-result.component.ts
+++ app/frontend/src/app/components/game-result/game-result.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-game-result',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './game-result.component.html',
   styleUrl: './game-result.component.scss'
 })
