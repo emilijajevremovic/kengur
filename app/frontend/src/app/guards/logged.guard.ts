b941f61e1c8dd7a64f09374e8337374c383c3b68
emilijajevremovic <jev.emilija@gmail.com>
2025-02-17 22:33:19 +0100

    test

--- app/frontend/src/app/guards/logged.guard.ts
+++ app/frontend/src/app/guards/logged.guard.ts
@@ -5,11 +5,12 @@ import { AuthService } from '../services/auth.service';
 export const loggedGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  
+
   if (typeof localStorage !== 'undefined' && authService.isLoggedIn()) {
     router.navigate(['/lobby']);
-    return false; 
+    return false;
   } else {
     return true;
   }
+  //
 };
